66984bdd887b88aab07ccb4bac990114687f8d37
hypercrafter <90453578+hypercrafter@users.noreply.github.com>
2021-12-04 18:06:47 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome! This is my developer website." />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          My website is at https://hypercraftermc.wixsite.com/home . </code>
         </p>
       </main>
 
