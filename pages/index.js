0103441d5fc2187d7bb06ea7b83a1718b5d83028
deathslinger13 <97905886+deathslinger13@users.noreply.github.com>
2022-01-17 22:36:29 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>InvisiDoods!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to the World of InvisiDoods!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Mint Price xyz<code>Launching Soon</code>
         </p>
       </main>
 
