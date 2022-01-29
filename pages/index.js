851e2f93265e734551fb4a8b894856c29bd2b767
ChrisinBarcelona <51531070+ChrisinBarcelona@users.noreply.github.com>
2022-01-29 16:56:50 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Bad Actors Studio!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
