08c9ecb00421589e08a6baa5fa940724945c8f59
Hussain, Siraj <Siraj.Hussain@wolterskluwer.com>
2022-04-27 02:48:25 +0500

    My first test commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -15,9 +15,10 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <p>This is my first test commit</p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
