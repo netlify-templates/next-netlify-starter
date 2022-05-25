e3906c13734a2d11cc4fe34b1e723a64e639ad29
Sumit Upadhyay <56456705+sumitupadhyay01@users.noreply.github.com>
2022-05-25 14:35:53 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my Next Js app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
