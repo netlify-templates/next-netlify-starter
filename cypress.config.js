5873a3766377bd9218da1b663f3ce72d1bda7403
Prince Wilson <maxcell.wilson@gmail.com>
2022-06-02 11:38:06 -0400

    fix: Tell Cypress no supportFile

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
