768e8033ba94f6628e3e2ada4c58d2e0ba5c176c
sfdc-innovator <110586308+sfdc-innovator@users.noreply.github.com>
2022-08-04 10:44:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          this is it!!
         </p>
       </main>
 
