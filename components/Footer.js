8ad9fd67bee7903abcae2cda893e762753fcb406
Tatyana <43764894+taty2010@users.noreply.github.com>
2023-03-08 09:39:46 -0600

    added new netlify logo to footer (#88)

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
       </footer>
     </>
   )
