7b497db3dd37485b806ff4226c639c5550957779
Yao-C <chengyao19920131@163.com>
2023-04-14 15:48:19 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <p>Hei there!</p>
       </main>
 
       <Footer />
