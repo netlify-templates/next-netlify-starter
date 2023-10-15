c80dd4dba2558308bdc952fd17cd15d2d6b6aabb
Lightneng <147958452+Lightneng@users.noreply.github.com>
2023-10-15 08:45:19 +0300

    index.js
    D

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,29 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hoş Geldiniz" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Merhaba, ben Arda.
+
+Uzun yıllardır yazılım alanında serbest çalışıyorum. Back-end ve front-end geliştirme konusunda uzmanım. Python, Java, C++, C#, Javascript, TypeScript, PHP, React, Svelte, Vue gibi programlama dillerini ve teknolojilerini kullanıyorum.
+
+Siber güvenlik alanında da akademik sertifikam bulunmaktadır. Bu alandaki bilgi ve deneyimlerimi kullanarak, web sitelerinizi ve uygulamalarınızı güvenli hale getirmenize yardımcı olabilirim.
+
+Sizin için yapabileceklerimin sınırı yok. Siz hayal edin, ben geliştireyim.
+
+İşte size sunabileceğim bazı hizmetler:
+
+Web siteleri ve mobil uygulamalar geliştirme
+Yazılım sistemleri ve uygulamaları geliştirme
+Otomasyon sistemleri geliştirme
+Veri analizi ve makine öğrenimi
+Siber güvenlik
+Benimle çalışmanın faydaları:
+
+Deneyimli ve uzman bir ekip
+Kaliteli ve güvenilir hizmet
+Esnek ve uygun fiyatlı çözümler
+Sizi daha iyi tanımak isterim. Birlikte harika şeyler başarabiliriz.</code>
         </p>
       </main>
 
