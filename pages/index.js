3b982f58617d4b23230973b0c964ed378974954a
MSahilH <sahilmonparah@gmail.com>
2023-10-25 22:13:44 +0530

    update favicon place

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,11 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>sahil Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="./images/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my sahil's app!" />
         <p className="description">
           Get started changing for testing. <code>pages/index.js</code>
         </p>
