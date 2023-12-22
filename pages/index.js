541ef63895d7f005283ed1c56bc415979e56ad5a
joseph mokoka <71907687+Joseph921223@users.noreply.github.com>
2023-12-22 03:17:23 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title="Welcome to Joseph app!" />
+        <p className="Software Developer Graduate">
+          I am glad, because i know how to make app <code>pages/index.js</code>
         </p>
       </main>
 
