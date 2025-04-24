01b3397d5d29ea567956b4e8437b42c3f280cf31
NghichTuyet <nghichtuyet293@gmail.com>
2025-04-24 18:01:12 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Con cừu ngã giữa đồng không dậy nổi" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
