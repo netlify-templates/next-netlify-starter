6c1548c9e8ad2b2ae6760ee2c9cb0923a9fc3222
Mastoura444 <47918438+Mastoura444@users.noreply.github.com>
2025-05-16 15:31:19 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to heat stress!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+    
       </main>
 
       <Footer />
