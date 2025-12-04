a9e9a9d8904d7c48c5b3e35ad82265b58c8dcf4a
TomasBankauskas <tbdizainas@gmail.com>
2025-12-04 11:51:37 +0200

    update dependencies

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,9 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <a href="https://www.netlify.com" target="_blank" rel="noopener">
+          <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        </a>
       </footer>
     </>
   )
